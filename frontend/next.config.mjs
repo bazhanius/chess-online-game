542d65332f2d3e5e1baa0fd3b56919e8a5a1a369
Alexander Bazhanov <30323120+bazhanius@users.noreply.github.com>
2026-01-25 17:33:58 +0300

    Delete frontend directory

--- frontend/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  assetPrefix: process.env.NEXT_PUBLIC_ASSET_PREFIX || '',
-  reactCompiler: true,
-  trailingSlash: true,
-};
-
-export default nextConfig;
